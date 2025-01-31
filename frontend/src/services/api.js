8f8d6680d7dc098b2d6c340cc9dd7de217c09728
Sunday Ochigbo <131619007+sunday004@users.noreply.github.com>
2025-01-31 06:43:13 -0600

    Update backend url in service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Base URL for the backend
 //const BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api';
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'https://mutualfundcalculator.onrender.com';
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'https://mutualfundcalculator.onrender.com/api';
 
 
 export const getMutualFunds = async () => {
